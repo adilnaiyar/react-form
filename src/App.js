6be075ee661ffab1438b2c9d05d38e3a427e129a
adilnaiyar <adil.naiyar@gmail.com>
2022-08-27 21:13:24 +0530

    useFormHook validations

--- src/App.js
+++ src/App.js
@@ -1,62 +1,23 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import "./App.css";
 
 function App() {
-  const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, SetIsSubmit] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({ ...formValues, [name]: value });
+  const [userInfo, setUserInfo] = useState();
+  const onSubmit = (data) => {
+    setUserInfo(data);
   };
-
-  useEffect(() => {
-    console.log(formErrors);
-    if (Object.keys(formErrors).length === 0 && isSubmit) {
-      console.log(formValues);
-    }
-  }, [formErrors]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validate(formValues));
-    SetIsSubmit(true);
-  };
-
-  const validate = (values) => {
-    const errors = {};
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
-
-    if (!values.username) {
-      errors.username = "UserName is Required!";
-    }
-
-    if (!values.email) {
-      errors.email = "Email is Required!";
-    } else if (!regex.test(values.email)) {
-      errors.email = "This is not a valid email format!";
-    }
-
-    if (!values.password) {
-      errors.password = "Password is required!";
-    } else if (values.password.length < 4) {
-      errors.password = "Password must be more than 4 characters";
-    } else if (values.password.length > 10) {
-      errors.password = "Password cannot exceed more than 10 characters";
-    }
-
-    return errors;
-  };
-
   return (
     <div className="container">
-      {Object.keys(formErrors).length === 0 && isSubmit ? (
-        <div className="message">Signed in successfully</div>
-      ) : (
-        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
-      )}
-      <form onSubmit={handleSubmit}>
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Login Form</h1>
         <div className="ui divider"></div>
         <div className="ui form">
@@ -67,39 +28,77 @@ function App() {
                 type="text"
                 name="username"
                 placeholder="Enter UserName"
-                value={formValues.username}
-                onChange={handleChange}
+                {...register("username", {
+                  required: "Username is required",
+                  minLength: {
+                    value: 4,
+                    message: "Username must be at least 3 characters",
+                  },
+                })}
               />
             </div>
           </div>
-          <span>{formErrors.username}</span>
+          {errors.username && <span>{errors.username.message}</span>}
           <div className="field">
             <label>Email</label>
             <div className="form-input">
               <input
                 type="text"
                 name="email"
-                placeholder="Enter UserName"
-                value={formValues.email}
-                onChange={handleChange}
+                placeholder="Enter Email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value:
+                      /^(([^<>()[\],;:\s@]+(\.[^<>()[\],;:\s@]+)*)|(.+))@(([^<>()[\],;:\s@]+\.)+[^<>()[\],;:\s@]{2,})$/i,
+                    message: "This is not a valid email",
+                  },
+                })}
               />
             </div>
           </div>
-          <span>{formErrors.email}</span>
+          {errors.email && <span>{errors.email.message}</span>}
           <div className="field">
             <label>Password</label>
             <div className="form-input">
               <input
                 type="password"
                 name="password"
-                placeholder="Enter UserName"
-                value={formValues.password}
-                onChange={handleChange}
+                placeholder="Enter Password"
+                {...register("password", {
+                  required: "Password is required",
+                  pattern: {
+                    value:
+                      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{6,}$/,
+                    message:
+                      "Must Contain 6 Characters, One Uppercase, One Lowercase,  One Number and one special case Character",
+                  },
+                })}
               />
             </div>
           </div>
-          <span>{formErrors.password}</span>
+          {errors.password && <span>{errors.password.message}</span>}
           <div>
+            <div className="field">
+              <label> Confirm Password</label>
+              <div className="form-input">
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  placeholder="Confirm Your Password"
+                  {...register("confirmPassword", {
+                    required: "Password Conformation is required",
+                    validate: (value) =>
+                      value === getValues("password") ||
+                      "Password doesn't match.",
+                  })}
+                />
+              </div>
+            </div>
+            {errors.confirmPassword && (
+              <span>{errors.confirmPassword.message}</span>
+            )}
+            <div></div>
             <button className="button">Submit</button>
           </div>
         </div>
